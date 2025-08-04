2a327908cee24827635ed899ba3aa9de64be8e30
vadim <sfileofficial@gmail.com>
2025-08-04 13:36:29 +0700

    added a new route "/data_to" + minor fixes

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,9 +1,18 @@
-print("Hello world")
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
-#декоратор для вывода страницы по умолчанию
+
 @app.route("/")
 def hello():
  return " <html><head></head> <body> Hello World! </body></html>"
+
+@app.route("/data_to")
+def data_to():
+ some_pars = {'user': 'Ivan', 'color': 'red'}
+ some_str = 'Hello my dear friends!'
+ some_value = 10
+ return render_template('simple.html',some_str = some_str, some_value = some_value,some_pars=some_pars)
+
 if __name__ == "__main__":
  app.run(host='127.0.0.1',port=5000)
