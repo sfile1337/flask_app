dd55962549ebd70e26292e248365131ed585ae9f
vadim <sfileofficial@gmail.com>
2025-08-06 03:46:59 +0700

    test

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -20,7 +20,7 @@ if res.ok:
 
 
 try:
- r = requests.get('http://localhost:5000/pixml')
+ r = requests.get('http://localhost:5000/apixml')
  print(r.status_code)
  if (r.status_code != 200):
   exit(1)
